cba0e1a8e9421bda736ffcc62e67b5ea5b11755d
Kalp <pakalp@sheridancollege.ca>
2019-01-24 17:05:36 -0500

    String datatype Revenue added

--- src/Content/TheStatic.java
+++ src/Content/TheStatic.java
@@ -2,6 +2,6 @@ package Content;
 
 public class TheStatic {
     public String name = "Fred";
-    
+    public String revenue = "$10 Billion";
     public static String company = "ABC Inc."; 
 }
--- src/demostatic/DemoStatic.java
+++ src/demostatic/DemoStatic.java
@@ -15,16 +15,18 @@ public class DemoStatic {
          System.out.println("One;s name is: " +one.name);
         System.out.println("Two's name is: " +two.name);
         
-        System.out.println("One's company is: "+one.company);
-        System.out.println("Two's company is: "+two.company);
+    //    System.out.println("One's company is: "+one.company);
+      //  System.out.println("Two's company is: "+two.company);
         
         one.company = "ACME Inc";
         System.out.println("One's company is: "+one.company);
         System.out.println("Two's company is: "+two.company);
         
         TheStatic.company = "Widgets";
-        System.out.println("One's company is: "+one.company);
-        System.out.println("Two's company is: "+two.company);
+        //System.out.println("One's company is: "+one.company);
+        //System.out.println("Two's company is: "+two.company);
+        
+        System.out.print("Company's revenue is: " +one.revenue);
         
        //  TheStatic.name= "Wilma";
        
